be2dbd7131d5fae77c6ae06d24e79170406cad3f
aslnbxrz <bexruz.aslonov1@gmail.com>
2025-09-30 12:38:58 +0500

    add OneIDLogout class for handling logout requests

---
+++ OneIDLogout.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Aslnbxrz\OneID;
+
+use Throwable;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use SocialiteProviders\Manager\ConfigTrait;
+
+final class OneIDLogout
+{
+    use ConfigTrait;
+
+    public function __invoke($accessTokenOrSessionId): void
+    {
+        $client = new Client();
+        try {
+            $client->post(rtrim($this->getConfig('base_url', 'https://sso.egov.uz'), '/') . '/sso/oauth/Authorization.do', [
+                RequestOptions::FORM_PARAMS => [
+                    'grant_type' => 'one_log_out',
+                    'client_id' => $this->getConfig('client_id'),
+                    'client_secret' => $this->getConfig('client_secret'),
+                    'access_token' => $accessTokenOrSessionId,
+                    'scope' => $this->getConfig('scope', 'one_code'),
+                ],
+                'headers' => ['Accept' => 'application/json'],
+            ]);
+        } catch (Throwable) {}
+    }
+}
